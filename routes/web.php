40ba8c7b49f1c7ffb267e1948bd8d2a1cfa8d0eb
yugo uchiida <y.uchiida@nirneege.com>
2022-04-01 19:49:21 +0900

    add: Livewire sample component

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LivewireTestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -11,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,9 +21,21 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+/* Livewireの動作確認用 */
+Route::controller(LivewireTestController::class)
+    ->prefix('livewire-test')->name('livewire-test.')->group(function () {
+    /* 1つめのindex にはprefixで'livewire-test'が、
+     * 2つめのindex にはRoute::controller() で LivewireTestController が自動的に付与される
+     * name() も、これを囲んでいる 上の階層のRoute でname('livewire-test.') が含まれているので、
+     * すべてのnameにlivewire-test. が先頭に付与される
+     */
+    Route::get('index', 'index')->name('index');
+    Route::get('register', 'register')->name('register');
 });
