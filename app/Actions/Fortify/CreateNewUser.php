98e43642125bec9c7759dafc6aeeece7abd909f4
yugo uchiida <y.uchiida@nirneege.com>
2022-08-31 02:49:08 +0900

    set role default values

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -31,6 +31,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'role' => 9, /* ユーザー追加時のデフォルト値として9(一般ユーザー権限)を指定 */
         ]);
     }
 }
