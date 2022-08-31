352e18185637719d01d3fa94deea9b2a91aca497
yugo uchiida <y.uchiida@nirneege.com>
2022-08-31 09:59:51 +0900

    define events table migration

--- database/migrations/2022_08_31_094046_create_events_table.php
+++ database/migrations/2022_08_31_094046_create_events_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->comment('イベント名');
+            $table->text('information')->comment('イベント詳細');
+            $table->integer('max_people')->comment('最大参加人数(予約枠数)');
+            $table->datetime('start_date')->comment('予約開始日時');
+            $table->datetime('end_date')->comment('予約終了日時');
+            $table->boolean('is_visible')->comment('表示可否');
             $table->timestamps();
         });
     }
