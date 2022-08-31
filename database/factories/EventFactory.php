1f3e8949cfa1b277fc43ca97818c5963cdb1efea
yugo uchiida <y.uchiida@nirneege.com>
2022-08-31 10:01:09 +0900

    define Event model factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,8 +16,16 @@ class EventFactory extends Factory
      */
     public function definition()
     {
+        $dummyDate = $this->faker->dateTimeThisMonth;
+
+        /* ランダムデータ生成時のルール */
         return [
-            //
+            'name' => $this->faker->name,
+            'information' => $this->faker->realText,
+            'max_people' => $this->faker->numberBetween(1,20), // 1 ~ 20 人まで予約できるデータを作る
+            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
+            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'), // 予約終了時刻が予約開始時刻よりも過去にならないように、 時間を加算して設定する
+            'is_visible' => $this->faker->boolean,
         ];
     }
 }
