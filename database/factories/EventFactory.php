818e4a50e42de69e49e74ee4562186f56e0c479d
yugo uchiida <y.uchiida@nirneege.com>
2022-09-01 15:46:33 +0900

    fix event factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,16 +16,24 @@ class EventFactory extends Factory
      */
     public function definition()
     {
-        $dummyDate = $this->faker->dateTimeThisMonth;
+        $availableHour = $this->faker->numberBetween(10, 18); //10時〜18時
+        $minutes = [0, 30]; // 00分か 30分
+        $mKey = array_rand($minutes); //ランダムにキーを取得
+        $addHour = $this->faker->numberBetween(1, 3); // イベント時間 1時間〜3時間
 
-        /* ランダムデータ生成時のルール */
+        $dummyDate = $this->faker->dateTimeThisMonth;
+        $startDate = $dummyDate->setTime($availableHour, $minutes[$mKey]);
+        $clone = clone $startDate;
+        $endDate = $clone->modify('+'.$addHour.'hour');
+        // dd($startDate, $endDate);
+
         return [
             'name' => $this->faker->name,
             'information' => $this->faker->realText,
-            'max_people' => $this->faker->numberBetween(1,20), // 1 ~ 20 人まで予約できるデータを作る
-            'start_date' => $dummyDate->format('Y-m-d H:i:s'),
-            'end_date' => $dummyDate->modify('+1hour')->format('Y-m-d H:i:s'), // 予約終了時刻が予約開始時刻よりも過去にならないように、 時間を加算して設定する
-            'is_visible' => $this->faker->boolean,
+            'max_people' => $this->faker->numberBetween(1,20),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'is_visible' => $this->faker->boolean
         ];
     }
 }
